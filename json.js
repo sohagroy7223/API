082d99151e5480d494bc26fbde0a334f44a71f46
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-01 11:38:48 +0600

    addad how to object to string by json and stringify to onject

---
+++ json.js
@@ -0,0 +1,18 @@
+const person = {
+  name: "sohag",
+  age: 20,
+  friends: ["akash", "hori", "ripon", "ashim"],
+  isSingle: true,
+};
+
+// console.log(person);
+
+// we can do, object ot stringify by JSON.stringify*****
+const newPerson = JSON.stringify(person);
+console.log(typeof newPerson);
+console.log(newPerson);
+
+// JSON.stringify to Object***********
+const person2 = JSON.parse(newPerson);
+console.log(typeof person2);
+console.log(person2);
