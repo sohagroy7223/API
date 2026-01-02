01dc98bdeaf03e90c3ef9a8bc8c5b00a0f4edb3f
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-02 11:10:14 +0600

    we can call any user any where any data

--- moreAPI.js
+++ moreAPI.js
@@ -1,5 +1,19 @@
 function userdata() {
   fetch("https://jsonplaceholder.typicode.com/users")
     .then((response) => response.json())
-    .then((obj) => console.log(obj[0].email));
+    .then((obj) => {
+      console.log(obj);
+      singleData(obj);
+      thirdUser(obj);
+    });
+}
+
+// arrow function*******
+const singleData = (firstUser) => {
+  console.log(firstUser[0]);
+};
+
+// normal user*********
+function thirdUser(thirdData) {
+  console.log(thirdData[2]);
 }
