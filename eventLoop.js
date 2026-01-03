cb7f2dfd6770f8c8877074447700d4d51edb605c
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-03 23:22:59 +0600

    addad Asynchronous vs Synchronous

---
+++ eventLoop.js
@@ -0,0 +1,28 @@
+const person1 = () => {
+  console.log("tea is ready 1");
+};
+const person2 = () => {
+  console.log("jalmuri is ready 2");
+};
+const person3 = () => {
+  setTimeout(() => {
+    console.log("fuksa is ready 3");
+  });
+};
+const person4 = () => {
+  console.log("tea is ready 4");
+};
+const person5 = () => {
+  setTimeout(() => {
+    console.log("fuksa is ready 5");
+  });
+};
+const person6 = () => {
+  console.log("jalmuri is ready 6");
+};
+person1();
+person2();
+person3();
+person4();
+person5();
+person6();
