cd19b2a055a4131003689c05f4c24ee9ea092972
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-03 19:24:05 +0600

    addad catch function if code has a error it is shoe in the catch error

--- async.js
+++ async.js
@@ -8,10 +8,23 @@
 
 // async function**************
 
-const handleUser2 = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await response.json();
-  console.log(data);
+// const handleUser2 = async () => {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const data = await response.json();
+//   console.log(data);
+// };
+
+// handleUser2();
+
+// if we find the error in catch*************
+const handleUser3 = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  } catch {
+    console.log("error aca");
+  }
 };
 
-handleUser2();
+handleUser3();
