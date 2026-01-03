9c45de8ed71d94bdde449566d8cdf9fa1363cf4b
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-03 19:10:29 +0600

    addad async function

--- async.js
+++ async.js
@@ -1,6 +1,17 @@
-const handleUser = () => {
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log("error dorce", error));
+// normal function********
+// const handleUser = () => {
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+//     .catch((error) => console.log("error dorce", error));
+// };
+
+// async function**************
+
+const handleUser2 = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  console.log(data);
 };
+
+handleUser2();
