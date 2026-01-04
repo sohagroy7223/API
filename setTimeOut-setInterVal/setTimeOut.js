306bc41170fdcff6ce4ef7c0784ce79186fb8520
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-04 17:08:50 +0600

    addad set time out function

---
+++ setTimeOut-setInterVal/setTimeOut.js
@@ -0,0 +1,9 @@
+console.log("1");
+console.log("2");
+console.log("3");
+
+setTimeout(() => {
+  console.log("amar sonar bangla");
+}, 3000); // 3 second por code ta run hobe, 1 bar
+console.log("4");
+console.log("5");
