eebe56eef72d92ae5c2f053de3291f5ebdcff5c1
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-04 11:22:37 +0600

    addad how to create promise with json function and without json function

--- promise.js
+++ promise.js
@@ -4,10 +4,56 @@ const loadData = () => {
     // .then(function (res) {
     //   return res.json();
     // });
-    .then((data) => console.log(data));
-  // .then(function (data) {
-  //   console.log(data);
-  // });
+    .then((data) => console.log(data))
+    // .then(function (data) {
+    //   console.log(data);
+    // });
+    // if any error in this function it is show in catch**
+    .catch((error) => console.log(error));
 };
 
 loadData();
+
+// how to create promise**************
+// without json****
+// const fetchData = () => {
+//   return new Promise((resolve, reject) => {
+//     const success = true;
+//     if (success) {
+//       resolve({ name: "sohag" });
+//     } else {
+//       reject("server error");
+//     }
+//   });
+// };
+
+// with json function**********
+
+const fetchData1 = () => {
+  return new Promise((resolve, reject) => {
+    const status = true;
+    if (status) {
+      const mokDAta = {
+        json: () => Promise.resolve({ name: "akash" }),
+      };
+      resolve(mokDAta);
+    } else {
+      reject("server error");
+    }
+  });
+};
+
+// check promise*********
+
+fetchData1()
+  .then(function (resolve) {
+    return resolve.json();
+  })
+
+  .then(function (data) {
+    console.log(data);
+  })
+
+  .catch(function (error) {
+    console.log(error);
+  });
