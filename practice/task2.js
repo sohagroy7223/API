3c74c92e0778061308c5d0e0ae2da32e794c898e
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-05 11:45:54 +0600

    addad a function that get 2 parameter and output after 2 second

---
+++ practice/task2.js
@@ -0,0 +1,11 @@
+// setTimeout(() => {
+//   console.log("hello sohag");
+// }, 2000);
+
+function dealGreeting(name, delay) {
+  setTimeout(() => {
+    console.log(name);
+  }, delay);
+}
+const name = "hello sohag";
+dealGreeting(name, 2000);
